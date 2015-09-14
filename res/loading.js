5b04daefdff19eed8c34267a6524cab3daec8d3c
johnnywatt <johnnywatt@foxmail.com>
2015-09-14 16:53:30 +0800

    loading
    new loading intergace

--- res/loading.js
+++ res/loading.js
@@ -1,1 +1,156 @@
-(function(){var createStyle=function(){return".cocosLoading{position:absolute;margin:-30px -60px;padding:0;top:50%;left:50%}"+".cocosLoading ul{margin:0;padding:0;}"+".cocosLoading span{color:#FFF;text-align:center;display:block;}"+".cocosLoading li{list-style:none;float:left;border-radius:15px;width:15px;height:15px;background:#FFF;margin:5px 0 0 10px}"+".cocosLoading li .ball,.cocosLoading li .unball{background-color:#2187e7;background-image:-moz-linear-gradient(90deg,#2187e7 25%,#a0eaff);background-image:-webkit-linear-gradient(90deg,#2187e7 25%,#a0eaff);width:15px;height:15px;border-radius:50px}"+".cocosLoading li .ball{transform:scale(0);-moz-transform:scale(0);-webkit-transform:scale(0);animation:showDot 1s linear forwards;-moz-animation:showDot 1s linear forwards;-webkit-animation:showDot 1s linear forwards}"+".cocosLoading li .unball{transform:scale(1);-moz-transform:scale(1);-webkit-transform:scale(1);animation:hideDot 1s linear forwards;-moz-animation:hideDot 1s linear forwards;-webkit-animation:hideDot 1s linear forwards}"+"@keyframes showDot{0%{transform:scale(0,0)}100%{transform:scale(1,1)}}"+"@-moz-keyframes showDot{0%{-moz-transform:scale(0,0)}100%{-moz-transform:scale(1,1)}}"+"@-webkit-keyframes showDot{0%{-webkit-transform:scale(0,0)}100%{-webkit-transform:scale(1,1)}}"+"@keyframes hideDot{0%{transform:scale(1,1)}100%{transform:scale(0,0)}}"+"@-moz-keyframes hideDot{0%{-moz-transform:scale(1,1)}100%{-moz-transform:scale(0,0)}}"+"@-webkit-keyframes hideDot{0%{-webkit-transform:scale(1,1)}100%{-webkit-transform:scale(0,0)}}"};var createDom=function(id,num){id=id||"cocosLoading";num=num||5;var i,item;var div=document.createElement("div");div.className="cocosLoading";div.id=id;var bar=document.createElement("ul");var list=[];for(i=0;i<num;i++){item=document.createElement("li");list.push({ball:document.createElement("div"),halo:null});item.appendChild(list[list.length-1].ball);bar.appendChild(item)}var span=document.createElement("span");span.innerHTML="Loading";div.appendChild(bar);div.appendChild(span);document.body.appendChild(div);return list};var startAnimation=function(list,callback){var index=0;var direction=true;var time=300;var animation=function(){setTimeout(function(){if(callback&&!callback()){return}var item=list[index];if(direction){item.ball.className="ball"}else{item.ball.className="unball"}index++;if(index>=list.length){direction=!direction;index=0;time=1000}else{time=300}animation()},time)};animation()};(function(){var bgColor=document.body.style.background;document.body.style.background="#000";var style=document.createElement("style");style.type="text/css";style.innerHTML=createStyle();document.head.appendChild(style);var list=createDom();startAnimation(list,function(){var div=document.getElementById("cocosLoading");if(!div){document.body.style.background=bgColor}return !!div})})()})();
+/* super inefficient right now, could be improved */
+var c = document.getElementById('c'),
+    ctx = c.getContext('2d'),
+    cw = c.width = 400,
+		ch = c.height = 300,
+    rand = function(a,b){return ~~((Math.random()*(b-a+1))+a);},
+    dToR = function(degrees){
+        return degrees * (Math.PI / 180);
+    },
+    circle = {
+      x: (cw / 2) + 5,
+      y: (ch / 2) + 22,
+      radius: 90,
+      speed: 2,
+      rotation: 0,
+      angleStart: 270,
+      angleEnd: 90,
+      hue: 220,
+      thickness: 18,
+      blur: 25
+    },
+    particles = [],
+    particleMax = 100,
+    updateCircle = function(){
+      if(circle.rotation < 360){
+      	circle.rotation += circle.speed;
+      } else {
+      	circle.rotation = 0; 
+      }
+    },
+    renderCircle = function(){
+      ctx.save();
+      ctx.translate(circle.x, circle.y);
+      ctx.rotate(dToR(circle.rotation));
+      ctx.beginPath();
+      ctx.arc(0, 0, circle.radius, dToR(circle.angleStart), dToR(circle.angleEnd), true);
+      ctx.lineWidth = circle.thickness;    
+      ctx.strokeStyle = gradient1;
+      ctx.stroke();
+      ctx.restore();
+    },
+    renderCircleBorder = function(){
+      ctx.save();
+      ctx.translate(circle.x, circle.y);
+      ctx.rotate(dToR(circle.rotation));
+      ctx.beginPath();
+      ctx.arc(0, 0, circle.radius + (circle.thickness/2), dToR(circle.angleStart), dToR(circle.angleEnd), true);
+      ctx.lineWidth = 2;  
+      ctx.strokeStyle = gradient2;
+      ctx.stroke();
+      ctx.restore();
+    },
+		renderCircleFlare = function(){
+      ctx.save();
+      ctx.translate(circle.x, circle.y);
+      ctx.rotate(dToR(circle.rotation+185));
+      ctx.scale(1,1);
+      ctx.beginPath();
+      ctx.arc(0, circle.radius, 30, 0, Math.PI *2, false);
+      ctx.closePath();
+      var gradient3 = ctx.createRadialGradient(0, circle.radius, 0, 0, circle.radius, 30);      
+      gradient3.addColorStop(0, 'hsla(330, 50%, 50%, .35)');
+      gradient3.addColorStop(1, 'hsla(330, 50%, 50%, 0)');
+      ctx.fillStyle = gradient3;
+      ctx.fill();     
+      ctx.restore();
+    },
+    renderCircleFlare2 = function(){
+      ctx.save();
+      ctx.translate(circle.x, circle.y);
+      ctx.rotate(dToR(circle.rotation+165));
+      ctx.scale(1.5,1);
+      ctx.beginPath();
+      ctx.arc(0, circle.radius, 25, 0, Math.PI *2, false);
+      ctx.closePath();
+      var gradient4 = ctx.createRadialGradient(0, circle.radius, 0, 0, circle.radius, 25);
+      gradient4.addColorStop(0, 'hsla(30, 100%, 50%, .2)');
+      gradient4.addColorStop(1, 'hsla(30, 100%, 50%, 0)');
+      ctx.fillStyle = gradient4;
+      ctx.fill();     
+      ctx.restore();
+    },
+    createParticles = function(){
+      if(particles.length < particleMax){
+        particles.push({
+          x: (circle.x + circle.radius * Math.cos(dToR(circle.rotation-85))) + (rand(0, circle.thickness*2) - circle.thickness),
+          y: (circle.y + circle.radius * Math.sin(dToR(circle.rotation-85))) + (rand(0, circle.thickness*2) - circle.thickness),
+          vx: (rand(0, 100)-50)/1000,
+          vy: (rand(0, 100)-50)/1000,
+          radius: rand(1, 6)/2,
+          alpha: rand(10, 20)/100
+        });
+      }
+    },
+    updateParticles = function(){
+      var i = particles.length;
+      while(i--){
+      	var p = particles[i];
+        p.vx += (rand(0, 100)-50)/750;
+        p.vy += (rand(0, 100)-50)/750;
+        p.x += p.vx;
+        p.y += p.vy;
+        p.alpha -= .01;
+        
+        if(p.alpha < .02){
+          particles.splice(i, 1)
+        }
+      }
+    },
+    renderParticles = function(){
+      var i = particles.length;
+      while(i--){
+      	var p = particles[i];
+        ctx.beginPath();
+        ctx.fillRect(p.x, p.y, p.radius, p.radius);
+        ctx.closePath();
+        ctx.fillStyle = 'hsla(0, 0%, 100%, '+p.alpha+')';
+      }
+    },
+    clear = function(){
+      ctx.globalCompositeOperation = 'destination-out';
+      ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+      ctx.fillRect(0, 0, cw, ch);
+      ctx.globalCompositeOperation = 'lighter';		
+    },
+    loop = function(){
+      clear();
+      updateCircle();
+      renderCircle();
+      renderCircleBorder();
+      renderCircleFlare();
+      renderCircleFlare2();
+      createParticles();
+      updateParticles();
+      renderParticles();
+    };
+
+/* Append Canvas */
+//document.body.appendChild(c);
+
+/* Set Constant Properties */
+ctx.shadowBlur = circle.blur;
+ctx.shadowColor = 'hsla('+circle.hue+', 80%, 60%, 1)';
+ctx.lineCap = 'round';
+  
+var gradient1 = ctx.createLinearGradient(0, -circle.radius, 0, circle.radius);
+gradient1.addColorStop(0, 'hsla('+circle.hue+', 60%, 50%, .25)');
+gradient1.addColorStop(1, 'hsla('+circle.hue+', 60%, 50%, 0)');
+  
+var gradient2 = ctx.createLinearGradient(0, -circle.radius, 0, circle.radius);
+gradient2.addColorStop(0, 'hsla('+circle.hue+', 100%, 50%, 0)');
+gradient2.addColorStop(.1, 'hsla('+circle.hue+', 100%, 100%, .7)');
+gradient2.addColorStop(1, 'hsla('+circle.hue+', 100%, 50%, 0)');
+
+/* Loop It, Loop It Good */
+setInterval(loop, 16);
